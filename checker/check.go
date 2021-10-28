4fd0cceae82f6323a6459644da75411a88c163f3
OmarElKhatibCS <omarelkhatib@mail.com>
2021-10-28 21:15:51 +0300

    Implement basic version of checker

--- checker/check.go
+++ checker/check.go
@@ -1,9 +1,90 @@
-package main
+package checker
 
 import (
-	"net/url"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
-func GetChunksLength(u url.URL) {
+var (
+	ErrNotValid = errors.New("the number of chuncks surpass MaxInt")
+)
 
+const (
+	maxUint = ^uint(0)
+	minUint = 0
+	maxInt  = int(maxUint >> 1)
+	minInt  = -maxInt - 1
+)
+
+// GetChunksLength will return the chunks length of the video
+// it will use MaxInt as highest possible chunk length
+func GetChunksLength(link string) (int, error) {
+	return GetChunksLengthWithMax(link, maxInt)
+}
+
+// GetChunksLength will return the chunks length of the video
+// it will use max parameter as highest possible chunk length
+func GetChunksLengthWithMax(link string, max int) (int, error) {
+	var low int
+	var mid int
+	var high int
+	var currHigh int
+
+	var sub_link string
+
+	low = minUint
+	high = max
+	currHigh = high
+
+	sub_link = strings.Split(link, "/chunked/")[0] + "/chunked/"
+	link = sub_link + strconv.Itoa(high) + ".ts"
+
+	if status, _ := getStatusCode(link); status == 200 {
+		return -1, ErrNotValid
+	}
+
+	for {
+		high = high / 2
+		link = sub_link + strconv.Itoa(high) + ".ts"
+		log.Println("Trying: " + link)
+
+		if status, _ := getStatusCode(link); status != 200 {
+			currHigh = high
+		} else {
+			high = currHigh
+			break
+		}
+	}
+	log.Println("Highest: " + link)
+
+	for {
+		mid = (high + low) / 2
+		link = sub_link + strconv.Itoa(mid) + ".ts"
+		log.Println("Trying: " + link)
+		if status, _ := getStatusCode(link); status == 200 {
+			low = mid
+		} else {
+			high = mid
+		}
+		if high == low+1 {
+			break
+		}
+	}
+
+	return low, nil
+}
+
+// getStatusCode will try and make an HTTP Get request to a giving URL
+// and return it's HTTP Status Code
+func getStatusCode(url string) (int, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return -1, err
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode, nil
 }
--- main.go
+++ main.go
@@ -2,35 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"strings"
+
+	"chunky.github.com/checker"
 )
 
 const (
-	u       = "https://d2nvs31859zcd8.cloudfront.net/8800ebf66a27286d9a6a_handmade_hero_40088786859_1634093554/chunked/0.ts"
-	MaxUint = ^uint(0)
-	MaxInt  = int(MaxUint >> 1)
+	u = "https://d2nvs31859zcd8.cloudfront.net/8800ebf66a27286d9a6a_handmade_hero_40088786859_1634093554/chunked/0.ts"
 )
 
 func main() {
-	var uu string
-	var chunk int = 9999
-
-	for {
-		uu = strings.Split(u, "/chunked/")[0] + "/chunked/" + fmt.Sprint(chunk) + ".ts"
-		resp, err := http.Get(uu)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != 200 {
-			chunk /= 2
-			fmt.Printf("Chunk %d : NO\n", chunk)
-			continue
-		}
-		fmt.Printf("chunck %d : exists\n", chunk)
-		chunk *= 2
-	}
-
+	length, _ := checker.GetChunksLengthWithMax(u, 10000)
+	fmt.Println(length)
 }
