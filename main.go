32d7edcde11f285c895c56b090806a56c236e18d
OmarElKhatibCS <omarelkhatib@mail.com>
2021-11-05 00:25:21 +0200

    adding very very basic, synchronal downloader

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"os"
 	"path"
+	"strconv"
 
 	"chunky.github.com/downloader"
 
@@ -22,7 +23,7 @@ func main() {
 	link := flag.String("url", "", `provide a link that have a chunk , example:
 https://d2nvs31859zcd8.cloudfront.net/70c102b5b66dbeac89e4_handmade_hero_40072241627_1633745055/chunked/155.ts
 `)
-	p := flag.String("dir", "", "sepcify a download path")
+	p := flag.String("dir", "", "specify a download path")
 	max := flag.Int("max", -1, "provide the excpected max number of files, zero or negative numbers will be treated as max int")
 	dwn := flag.Bool("dwn", true, "by default true , false if you just want to get chunks size without downloading files")
 
@@ -70,10 +71,12 @@ func run(log *log.Logger, cfg Config) error {
 
 	log.Printf("Nb of chunks is %d", nbChunks)
 	if cfg.Dwn {
-		d := downloader.NewFileDownloaderWithLog(log, cfg.Link, "test.ts", cfg.Path)
-		err = d.Download()
-		if err != nil {
-			return err
+		for i := 0; i <= nbChunks; i++ {
+			d := downloader.NewFileDownloaderWithLog(log, cfg.Link, strconv.Itoa(i)+".ts", cfg.Path)
+			err = d.Download()
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
