95fda68ad9b64610dc53ced66e55907c9014c0b6
OmarElKhatibCS <omarelkhatib@mail.com>
2021-11-04 23:32:40 +0200

    Adding logs to downloader and path where file should be placed with handling creating the Directories

--- downloader/FileDownloader.go
+++ downloader/FileDownloader.go
@@ -1,26 +1,51 @@
 package downloader
 
 import (
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"path"
 )
 
 type FileDownloader struct {
 	Url  string
 	Name string
+	Path string
+	Log  *log.Logger
 }
 
-func NewFileDownloader(url, name string) *FileDownloader {
+func NewFileDownloader(url, name, path string) *FileDownloader {
 	return &FileDownloader{
 		Url:  url,
 		Name: name,
+		Path: path,
+		Log:  log.New(io.Discard, "", 0),
+	}
+}
+
+func NewFileDownloaderWithLog(log *log.Logger, url, name, path string) *FileDownloader {
+	return &FileDownloader{
+		Url:  url,
+		Name: name,
+		Path: path,
+		Log:  log,
 	}
 }
 
 func (fd *FileDownloader) Download() error {
+	err := os.MkdirAll(fd.Path, 0755)
+	if err != nil {
+		return err
+	}
 	resp, err := http.Get(fd.Url)
 	defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	fd.Log.Printf("Downloading %s to %s\n", fd.Name, fd.Path)
 
 	file, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -30,9 +55,7 @@ func (fd *FileDownloader) Download() error {
 	if err != nil {
 		return err
 	}
-	err = os.WriteFile(fd.Name, file, 0664)
-	if err != nil {
-		return err
-	}
-	return nil
+	p := path.Join(fd.Path, fd.Name)
+
+	return os.WriteFile(p, file, 0664)
 }
